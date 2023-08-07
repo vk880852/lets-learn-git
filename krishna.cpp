501386de54dec67cf8e8706f438232dd97548836
Satyendra Kumar Rawat <vk880852@gmail.com>
2023-08-07 10:23:40 +0530

    I made another file

---
+++ krishna.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long
+#include<vector>
+#include<map>
+using namespace std;
+const ll mod=1e9+7;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin>>t;
+while(t--)
+{
+    cout<<"ram\n";
+}
+
+return 0;
+}
